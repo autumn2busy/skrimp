451f9993f6451e4c73541cf6517dd9e4c19a19c5
SonataStark <autumn.s.williams@gmail.com>
2018-01-05 13:49:16 -0500

    Add files via upload

---
+++ array.js
@@ -0,0 +1,29 @@
+/* var myscriptarray = [];
+
+myscriptarray[0]=30;
+myscriptarray[1]=10;
+myscriptarray[2]=5;
+
+shorthand add the values/create the index directly into the variable
+*/
+
+var myscriptarray=[30,10,50,60,40,90];
+
+var sum = 10;
+
+for (var i = 0 ; i < myscriptarray.length ; i++) {
+  sum = sum + myscriptarray[i];
+}
+
+//once i is no longer less than or equal to 6, perform this alert
+alert("The sum of your thing is " + sum);
+//answer is 280!!!
+
+/*
+when you call for the length on a string, it returns the number of characters
+in the string.
+However, when you call the .length on an array, it returns the number of positions
+in the ARRAY */
+
+30
+10
---
+++ testout.js
@@ -0,0 +1,6 @@
+
+var phrase = "Don't mix your quotes.";
+
+var mixedphrase = "He said \"that's fine,\" and left.";
+
+alert(mixedphrase.toUpperCase());
